383833011a0d367c688f09186ee1b871a03d8159
Hariharan Anantharaman <hariharan.anantharaman@techolution.com>
2018-04-01 17:31:55 +0530

    reveretd back old mistaken commit

--- supplydataservice/src/main/java/com/techolution/mauritius/smartwater/supply/repository/ConnectionDetailsRepository.java
+++ supplydataservice/src/main/java/com/techolution/mauritius/smartwater/supply/repository/ConnectionDetailsRepository.java
@@ -1,17 +1,15 @@
 package com.techolution.mauritius.smartwater.supply.repository;
 
 
-
-
-import javax.management.Notification;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import com.techolution.mauritius.smartwater.supply.domain.MeterConnection;
 
 
 
 @Repository
 
-public interface ConnectionDetailsRepository extends  CrudRepository<Notification,Long> {
+public interface ConnectionDetailsRepository extends  CrudRepository<MeterConnection,Long> {
 
 }
