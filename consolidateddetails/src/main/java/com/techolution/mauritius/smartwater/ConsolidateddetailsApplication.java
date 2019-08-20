f7ce39a47f77e864a1efe4d3e972b235fc938aa3
Hariharan Anantharaman <hariharan.anantharaman@techolution.com>
2019-08-20 18:45:47 +0530

    changes for gke implementaiton

--- consolidateddetails/src/main/java/com/techolution/mauritius/smartwater/ConsolidateddetailsApplication.java
+++ consolidateddetails/src/main/java/com/techolution/mauritius/smartwater/ConsolidateddetailsApplication.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @EnableJpaRepositories
 public class ConsolidateddetailsApplication {
--- consolidateddetails/src/main/java/com/techolution/mauritius/smartwater/controller/ConnectionMetaDataController.java
+++ consolidateddetails/src/main/java/com/techolution/mauritius/smartwater/controller/ConnectionMetaDataController.java
@@ -22,7 +22,7 @@ import com.techolution.mauritius.smartwater.service.ConsolidatedDataService;
 
 @CrossOrigin(origins = {"*"})
 @RestController
-@RequestMapping(value="/read")
+@RequestMapping(value="/readconsolidated")
 
 public class ConnectionMetaDataController {
 	
