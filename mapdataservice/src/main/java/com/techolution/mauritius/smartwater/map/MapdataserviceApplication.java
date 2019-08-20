69b0ca0d6684481f5e40b0e695141a3fb6cb2018
Hariharan Anantharaman <hariharan.anantharaman@techolution.com>
2019-08-20 19:07:22 +0530

    Disabled eureka

--- mapdataservice/src/main/java/com/techolution/mauritius/smartwater/map/MapdataserviceApplication.java
+++ mapdataservice/src/main/java/com/techolution/mauritius/smartwater/map/MapdataserviceApplication.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class MapdataserviceApplication {
 
