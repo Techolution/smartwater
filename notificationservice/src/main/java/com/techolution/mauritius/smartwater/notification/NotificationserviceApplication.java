9657e4f4fe67cd92643659dcc4ca87dc9e171355
Hariharan Anantharaman <hariharan.anantharaman@techolution.com>
2019-08-20 19:15:15 +0530

    changes for gke

--- notificationservice/src/main/java/com/techolution/mauritius/smartwater/notification/NotificationserviceApplication.java
+++ notificationservice/src/main/java/com/techolution/mauritius/smartwater/notification/NotificationserviceApplication.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class NotificationserviceApplication {
 
