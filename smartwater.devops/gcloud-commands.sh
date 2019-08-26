10abeeb69f8497715d8ea963ef7816ab60533b26
Manish-Techolution <manish.bhakuni@techolution.com>
2019-08-27 01:32:01 +0700

    Added devops files for smart water kubernetes deployment and update

---
+++ smartwater.devops/gcloud-commands.sh
@@ -0,0 +1,5 @@
+## Set current working project in gcloud cli
+gcloud config set project synthetic-eon-241312
+
+## Setup kubernetes cluster
+gcloud container clusters get-credentials smart-water --region=us-central1-a
---
+++ smartwater.devops/kubectl-commands.sh
@@ -0,0 +1,6 @@
+## set kubectl context
+kubectl config use-context gke_synthetic-eon-241312_us-central1-a_smart-water
+
+## check current kubectl context
+kubectl config current-context
+
