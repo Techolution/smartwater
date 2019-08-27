21378a06ceac4fb407ab765d3ea7cf8e56b9d7a7
Hariharan Anantharaman <hariharan.anantharaman@techolution.com>
2019-08-27 19:08:53 +0530

    changes to disable discovery

--- mapdataservice/src/main/java/com/techolution/mauritius/smartwater/map/MapdataserviceApplication.java
+++ mapdataservice/src/main/java/com/techolution/mauritius/smartwater/map/MapdataserviceApplication.java
@@ -1,16 +1,7 @@
 package com.techolution.mauritius.smartwater.map;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 //@EnableDiscoveryClient
 @SpringBootApplication
 public class MapdataserviceApplication {
@@ -19,7 +10,7 @@ public class MapdataserviceApplication {
 		SpringApplication.run(MapdataserviceApplication.class, args);
 	}
 	
-	@RestController
+	/*@RestController
 	class ServiceInstanceRestController {
 
 	    @Autowired
@@ -30,5 +21,5 @@ public class MapdataserviceApplication {
 	            @PathVariable String applicationName) {
 	        return this.discoveryClient.getInstances(applicationName);
 	    }
-	}
+	}*/
 }
