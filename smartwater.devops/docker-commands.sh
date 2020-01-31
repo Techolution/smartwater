6b4e0f104d360be6a25c2681a024db57ce24162d
Manish-Techolution <manish.bhakuni@techolution.com>
2020-01-31 17:48:28 +0530

    Added scripts for building and deploying docker images

--- smartwater.devops/docker-commands.sh
+++ smartwater.devops/docker-commands.sh
@@ -1,4 +1,8 @@
 docker login -u _json_key -p "$(cat /Users/techo-admin/Documents/Techolution/smartwater.devops/service-accounts/gcr/synthetic-eon-241312-5981fa843f37.json)" https://gcr.io
+docker login -u _json_key -p "$(cat /Users/techo-admin/Documents/Techolution/smartwater/smartwater.anthos.devops/anthos-lab-usdc-8b65fe1d7b61.json)" https://asia.gcr.io
+
+## Authenticate docker with gcloud credentials
+gcloud auth configure-docker
 
 ## docker command for connectionstatistics micro-service
 docker build --file Dockerfile --tag gcr.io/synthetic-eon-241312/smartwater/connectionstatistics:2 .
@@ -39,6 +43,12 @@ kubectl -n dev set image deployment supplydataservice supplydataservice=gcr.io/s
 ## docker command for smart-water ui service
 docker build --file Dockerfile --tag gcr.io/synthetic-eon-241312/smartwater/ui:2 .
 docker push gcr.io/synthetic-eon-241312/smartwater/ui:2
+docker run -it gcr.io/synthetic-eon-241312/smartwater/ui:2
+kubectl -n dev set image deployment ui ui=gcr.io/synthetic-eon-241312/smartwater/ui:2
+
+## docker command for smart-water ui service
+docker build --file Dockerfile --tag asia.gcr.io/anthos-lab-usdc/smartwater/ui:21 .
+docker push asia.gcr.io/anthos-lab-usdc/smartwater/ui:21
 docker run -it gcr.io/synthetic-eon-241312/smartwater/ui:2
 kubectl -n dev set image deployment ui ui=gcr.io/synthetic-eon-241312/smartwater/ui:2
 
--- smartwater.devops/gcloud-commands.sh
+++ smartwater.devops/gcloud-commands.sh
@@ -1,5 +1,7 @@
 ## Set current working project in gcloud cli
 gcloud config set project synthetic-eon-241312
+gcloud config set project anthos-lab-usdc
+gcloud config get-value project
 
 ## Setup kubernetes cluster
 gcloud container clusters get-credentials smart-water --region=us-central1-a
